a7a6d50c8e79361d2d45fa6112e121925fc14eb7
Max Hayashi Batista <rm563717@fiap.com.br>
2025-11-03 08:20:17 -0300

    feat: estruturação básica do sistema usando next.js

---
+++ 1tdspk-602-nextjs-produtos/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
